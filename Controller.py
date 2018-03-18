5dc4538080b9fb2a16fa411368524e64fa74c707
Anastasia Brovchenko <brovchenko03051999@gmail.com>
2018-03-19 01:18:03 +0200

    first try MVC

---
+++ Controller.py
@@ -0,0 +1,9 @@
+# вся інша фігня
+
+
+class PaintController:
+    def __init__(self, model):
+        self.model = model
+
+    def setView(self, view):
+        self.view = view
---
+++ Model.py
@@ -0,0 +1,9 @@
+# всі дані
+
+default_color = "black"
+default_brush_size = 2
+
+class PaintModel():
+
+    def setCursor(self, place):
+        pass
